31da17a3d72402c3585948b866a6760bfb6d4443
SaaFomenko <s.fomenko@list.ru>
2022-11-28 01:31:16 +0300

    Save change define.h and figure.cpp

--- define.h
+++ define.h
@@ -4,9 +4,8 @@ class Figure
 {
 	private:
 		const int sides = 0;
-		const std::string name = "Фигура: ";
 	public:
-		const std::string lable = "Количество сторон: ";
+		static const std::string name;
 		virtual std::string getSides();
 };
 
@@ -14,16 +13,67 @@ class Triangle : public Figure
 {
 	private:
 		const int sides = 3;
-		const std::string name = "Треугольник: ";
+	
+	protected:
+		const int a = 10;
+		const int b = 20;
+		const int c = 30;
+		const int A = 50;
+		const int B = 60;
+		const int C = 70;
+
 	public:
+		static const std::string name;
+		static const std::string sides_lable;
+		static const std::string angles_lable;
+		static const std::string a_lable;
+		static const std::string b_lable;
+		static const std::string c_lable;
+		static const std::string A_lable;
+		static const std::string B_lable;
+		static const std::string C_lable;
+		
 		std::string getSides() override;
+		virtual std::string getStr();
 };
 
-class Quadrangle : public Triangle
+class Quadrangle : protected Triangle
 {
 	private:
 		const int sides = 4;
-		const std::string name = "Четырехугольник: ";
+
+	protected:
+		const int d = 40;
+
 	public:
+		static const std::string name;
+
 		std::string getSides() override;
+		std::string getStr() override;
 };
+
+class RightTriangle : public Triangle
+{
+	protected:
+		int C = 90;
+
+	public:
+		static const std::string name;
+
+		std::string getStr() override;
+};
+
+class IsoscelesTriangle : public Triangle
+{
+	protected:
+		int a = 10;
+		int c = a;
+		int C = 90;
+
+	public:
+		static const std::string name;
+
+		std::string getStr() override;
+};
+
+
--- figure.cpp
+++ figure.cpp
@@ -1,6 +1,15 @@
 #include <string>
 #include "define.h"
 
+
+
+const std::string Triangle::name = "Треугольник: ";
+const std::string RightTriangle::name = "Прямоугольный треугольник: ";
+const std::string IsoscelesTriangle::name = "Равнобедренный треугольник: ";
+//const std::string IsoscelesTriangle::name = "Равнобедренный треугольник: ";
+const std::string Triangle::sides_lable = "Стороны: ";
+const std::string Triangle::angles_lable = "Углы: ";
+
 std::string Figure::getSides()
 {
 	const std::string s = std::to_string(sides);
