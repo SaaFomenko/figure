bed0e021fb098d99f5de2554608733935c1268bf
SaaFomenko <s.fomenko@list.ru>
2022-12-05 20:38:32 +0300

    Save add all class in define.h

--- define.h
+++ define.h
@@ -128,3 +128,92 @@ class RightTriangle : public Triangle
 		virtual void checking() override;
 		virtual const std::string getStr() override;
 };
+
+class IsoscelesTriangle : public Triangle
+{
+	private:
+		const std::string name = "Равнобедренный треугольник";
+
+	protected:
+		bool check = true;
+		int c = a;
+		int C = A;
+
+	public:
+		static const std::string name;
+
+		std::string getStr() override;
+};
+
+class IquilateralTriangle : public Triangle
+{
+	protected:
+		const int a = 30;
+		const int b = a;
+		const int c = a;
+		const int A = 60;
+		const int B = A;
+		const int C = A;
+
+	public:
+		static const std::string name;
+
+		std::string getStr() override;
+};
+
+class RightQuadrangle : public RightTriangle
+{
+	protected:
+		const int c = a;
+		const int d = b;
+		const int A = C;
+		const int B = C;
+		const int D = C;
+
+	public:
+		static const std::string name;
+
+		std::string getStr() override;
+};
+
+class Quadrate : public RightQuadrangle
+{
+	protected:
+		const int a = b;
+		const int c = b;
+
+	public:
+		static const std::string name;
+
+		std::string getStr() override;
+};
+
+class Parallelogram : public Quadrate
+{
+	protected:
+		const int a = 20;
+		const int b = 30;
+		const int c = a;
+		const int d = b;
+		const int A = 30;
+		const int B = 40;
+		const int C = A;
+		const int D = B;
+
+	public:
+		static const std::string name;
+
+		std::string getStr() override;
+};
+
+class Rhomb : public Parallelogram
+{
+	protected:
+		const int a = b;
+		const int c = b;
+		
+	public:
+		static const std::string name;
+
+		std::string getStr() override;
+};
