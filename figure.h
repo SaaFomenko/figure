32b976fa41bfbb33da92ac67ae3ef92cb3cd4ddb
SaaFomenko <s.fomenko@list.ru>
2022-12-09 07:20:41 +0300

    Create file view.h with struct view.

--- figure.h
+++ figure.h
@@ -26,17 +26,6 @@ class Figure
 		Figure(std::string, int);
 		~Figure();
 
-		static const std::string sides;
-		static const std::string angles;
-		static const std::string a;
-		static const std::string b;
-		static const std::string c;
-		static const std::string d;
-		static const std::string A;
-		static const std::string B;
-		static const std::string C;
-		static const std::string D;
-
 		std::string getName();
 
 		int getCountSides();
---
+++ view.h
@@ -0,0 +1,13 @@
+struct view
+{
+	static const std::string sides;
+	static const std::string angles;
+	static const std::string a;
+	static const std::string b;
+	static const std::string c;
+	static const std::string d;
+	static const std::string A;
+	static const std::string B;
+	static const std::string C;
+	static const std::string D;
+}
